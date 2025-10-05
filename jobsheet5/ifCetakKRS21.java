d1262db9f767fff6d56c25de83880f0f5d2f9dc2
Fauziiiiii <fauzigoendoet99@gmail.com>
2025-10-05 13:25:03 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS21.java
+++ jobsheet5/ifCetakKRS21.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class ifCetakKRS21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String pesan;
 
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT udah lunas? (true/false) ");
         boolean uktLunas = sc.nextBoolean();
 
-        if(uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        }else{
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        // if(uktLunas){
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        // }else{
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // }
+
+        pesan = (uktLunas)
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println("--- Ouput dengan Ternary Operator ---");
+        System.out.println(pesan);
 
         sc.close();
     }
