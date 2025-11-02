d6ba9f77d6d414fc645a0fd2f4a54428847c4767
Fauziiiiii <fauzigoendoet99@gmail.com>
2025-11-02 14:04:59 +0700

    Percobaan 4

---
+++ jobsheet8/NilaiKelompok21.java
@@ -0,0 +1,29 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class NilaiKelompok21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai = 0;
+        float totalNilai, rataNilai = 0;
+
+        int i = 1;
+        while(i <= 6){
+            System.out.println("Kelompok " + i);
+
+            totalNilai = 0;
+            for(int j = 1; j <= 5; j++){
+                System.out.print("  Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+
+            i++;
+        }
+    }
+}
