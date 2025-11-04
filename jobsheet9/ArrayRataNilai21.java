454ca583bc0ed7bb5e6228e68f6a2a85876c90ae
Fauziiiiii <fauzigoendoet99@gmail.com>
2025-11-04 10:59:50 +0700

    Modifikasi Pertanyaan 1 Percobaan 3

--- jobsheet9/ArrayRataNilai21.java
+++ jobsheet9/ArrayRataNilai21.java
@@ -9,6 +9,7 @@ public class ArrayRataNilai21 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int totalMhsLulus = 0;
 
         for(int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
@@ -16,10 +17,15 @@ public class ArrayRataNilai21 {
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
+            if(nilaiMhs[i] > 70 && nilaiMhs[i] <= 100){
+                totalMhsLulus += 1;
+            }
             total += nilaiMhs[i];
         }
 
         rata2 = total / nilaiMhs.length;
+
+        System.out.println("Total mahasiswa yang lulus = " + totalMhsLulus);
         System.out.println("Rata-rata nilai = " + rata2);
     }
 }
