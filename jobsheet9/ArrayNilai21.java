0a90066de9d753eac63e39db378de5d83b33c134
Fauziiiiii <fauzigoendoet99@gmail.com>
2025-11-04 10:31:44 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai21.java
+++ jobsheet9/ArrayNilai21.java
@@ -8,13 +8,21 @@ public class ArrayNilai21 {
 
         int[] nilaiAkhir = new int[10];
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if(nilaiAkhir[i] > 70 && nilaiAkhir[i] <= 100){
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }else if(nilaiAkhir[i] >= 0  && nilaiAkhir[i] <= 70){
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }else{
+                System.out.println("Nilai mahasiswa ke-" + i + " tidak valid!");
+            }
         }
+
+        sc.close();
     }
 }
